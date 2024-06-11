954e14f78c8519408c4920aded6670c2e564262d
artchous <artchana123@hotmail.com>
2024-06-11 10:33:51 +0200

    modif dans service pour recevoir les topics

--- src/app/pages/cari-topic-posts-page/cari-topic-posts-page.component.ts
+++ src/app/pages/cari-topic-posts-page/cari-topic-posts-page.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnChanges, SimpleChanges} from '@angular/core';
 import {TopicPostDisplay} from '../../models/topic-post-display.model';
 import {TopicsPostsService} from '../../services/topics-posts.service';
 
@@ -7,17 +7,34 @@ import {TopicsPostsService} from '../../services/topics-posts.service';
   templateUrl: './cari-topic-posts-page.component.html',
   styleUrl: './cari-topic-posts-page.component.css',
 })
-export class CariTopicPostsPageComponent {
+export class CariTopicPostsPageComponent implements OnChanges {
   public allTopicPostsDisplay: TopicPostDisplay[] =
     [new TopicPostDisplay(), new TopicPostDisplay(), new TopicPostDisplay(),
       new TopicPostDisplay(), new TopicPostDisplay(),new TopicPostDisplay()];
 
   constructor(private topicsPostsService: TopicsPostsService) {
+    this.getTopics();
   }
 
   comment():void {
     let newWin = window.open("about:blank", "hello","width=250,height=50");
     // @ts-ignore
     newWin.document.write("Ne fonctionne pas encore");
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+  }
+
+  private getTopics() {
+    this.topicsPostsService.getTopicPostDisplay().subscribe(
+      {
+        next: result => {
+           console.log(result);
+        },
+        error: err => {
+          console.log(err);
+        }
+      }
+    )
   }
 }
--- src/app/services/topics-posts.service.ts
+++ src/app/services/topics-posts.service.ts
@@ -1,15 +1,21 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {TopicPostDisplay} from "../models/topic-post-display.model";
 
 @Injectable()
 export class TopicsPostsService  {
+
+  public topicsDisplay: TopicPostDisplay[];
 
   constructor(private httpClient: HttpClient) {
   }
 
   // Add Get Service
-
+  public getTopicPostDisplay(): Observable<TopicPostDisplay[]> {
+    let getUrl = 'http://127.0.0.1:9000/topics';
+    return this.httpClient.get<TopicPostDisplay[]>(getUrl);
+  }
 
   // Add Post Service here
 
